451bed18d8833b7611c36d647a06e9d52a6ad826
Oleg Selivanov <alien3569@mail.ru>
2022-12-24 09:22:28 +0300

    Add

--- Task/Program.cs
+++ Task/Program.cs
@@ -740,7 +740,7 @@ ReleaseMatrix(matrix);
 Задайте двумерный массив. Найдите сумму элементов, находящихся на 
 главной диагонали (с индексами (0,0); (1,1); и т.д.)
 */
-
+/*
 void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -776,4 +776,4 @@ Console.WriteLine("Начальная матрица: ");
 InputMatrix(matrix);
 Console.Write("Результат: ");
 Console.WriteLine(ReleaseMatrix(matrix));
-
+*/
